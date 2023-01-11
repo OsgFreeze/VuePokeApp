81bbd5366a5a90e2bf24bd71e30fa92599533e5e
OsgFreeze <noah@pilchowski.net>
2023-01-11 13:41:56 +0100

    1.0.4

---
+++ src/components/displayData.vue
@@ -0,0 +1,48 @@
+<template>
+    <div> Pokemon Nummer eingeben: 
+        <input id="eingabeId" type="number" v-model="PokeNumber"> </div>
+    
+      <div class="buttons">
+        <button @click="setNrPlus" > Hoch ↑ </button>
+        <button @click="setNrMinus"> Runter ↓ </button> 
+        &nbsp;
+        <button @click="getData"> Eingabe </button>
+      </div>
+    
+    <SearchBar/>
+    {{ PokemonName }}
+
+</template>
+
+<script>
+    import getPokeData from './getPokeData.vue';
+    import searchBar from './searchBar.vue';
+
+    export default {
+        name: "displayData",
+
+    Components: {
+        searchBar
+    },
+    data(){
+        return{
+            PokeNumber: 1,
+        }
+    },
+    methods: {
+        /*async setNrPlus() {
+            this.PokeNumber = (this.PokeNumber + 1);
+            this.loadApiNumber();
+        },
+        async setNrMinus() {
+            this.PokeNumber = (this.PokeNumber - 1);
+            this.loadApiNumber();
+        },*/
+        getData(){
+            const Pokemon = getPokeData.loadApiNumber(5);
+            return Pokemon;
+        }
+    },
+    }   
+
+</script>
--- src/components/getPokeData.vue
+++ src/components/getPokeData.vue
@@ -1,16 +1,25 @@
 <template>
-    <div>
-        <ul class="getPokeData">  </ul>
+    <div class="getPokeData">
+        <button @click="loadApiNumber">Click..</button>
     </div>
 
 </template>
+
 <script>
+import axios from 'axios'
+
 export default {
     name: 'getPokeData',
     components: {
+
+    },
+    data(){ 
+    return {
+      PokeNumber: 1,
+    }
     },
     methods: {
-        async loadApiNumber(pokeNumber){
+        async loadApiNumber(){
             await axios.get(`https://pokeapi.co/api/v2/pokemon/${this.PokeNumber}`).then((response) => {
                 console.log(response);
                 const data = response.data;
