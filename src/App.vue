5d6342f34b34156d53bc3440aa94189c2459c8fe
Hujary <petzold.tom2000@gmail.com>
2023-01-12 10:20:49 +0100

    a

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,19 @@
-<template>
+<template>   
 
-  <getPokeData/>   <!-- rufe getPokeData auf für ein API CALL -->
-
-  <div>   <!-- childComponent test für ein Provide/Inject  -->
-    <button @click="gespeicherterWert++" > press me -> [info +1 -> Provide/Inject] </button>
-    <pictureData/> 
-  </div>
-
-  <div>   <!-- childComponent test für ein props CALL -->
-    <button> press me [Props +1] </button>
-    <propsCallTest/> 
-  </div>
+  <propsCallTest/>
 
 </template>
 
 <script> 
-import axios from 'axios'; //Axios für API CALL importieren
-import getPokeData from './components/getPokeData.vue';
-import pictureData from './components/pictureData.vue';
+
 import propsCallTest from './components/propsCallTest.vue';
 
 export default {
   name: 'App',
   components: { //Komponenten deklarieren
-    getPokeData,
-    pictureData,
     propsCallTest
   }, 
 
-
-  data(){  //Variablen [Daten] lokal deklarieren
-    return {
-      gespeicherterWert: 5, //Variable -> [Type: Number]
-    }
-  },
-
-  methods: {
-        async getApiDataMain(){ //Api Aufruf -> (wichtig: API Objekt kann nur hier variablen zugewiesen werden)
-            await axios.get(`https://pokeapi.co/api/v2/pokemon/ditto}`).then((response) => {
-                console.log(response); 
-                const data = response.data; 
-                return data;
-            })
-        },
-  },
-
-  provide() { //Methode für den [provide & Inject]
-    const variable = {}; 
-    Object.defineProperty(variable, 'Nachricht', {   //kp wie man das weg bekommt ??
-      enumerable: true,
-      get: () => this.gespeicherterWert
-    } )
-    return {variable}
-  },
 }
 </script>
 
--- src/components/propsCallTest.vue
+++ src/components/propsCallTest.vue
@@ -1,22 +1,16 @@
 <template>  <!-- ref="divRef"  -->
-    <div ref="divRef"> 
-      <h1> Hallo Welt -> Props </h1>
-      {{this.gespeicherterWert}}
+    <div> 
+        <input type="text" ref="myRef" /> 
     </div>
+    
   </template>
   
   <script>
       export default {
       name: 'propsCallTest', //Component Name
 
-      data(){  //Variablen [Daten] lokal deklarieren
-    return {
-      gespeicherterWert: 0, //Variable -> [Type: Number]
-    }
-  },
-
-     mounted() {
-        console.log( this.$refs.divRef.getApiDataMain() ); 
+     mounted() {   
+          console.log(this.$refs.postListRef)   
     }    
   }   
   
