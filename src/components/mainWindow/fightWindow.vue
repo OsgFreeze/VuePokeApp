4292be0f8820cc364f3bbdecce20b68579f63a1b
Hujary <petzold.tom2000@gmail.com>
2023-01-15 18:30:46 +0100

    a

--- src/components/mainWindow/fightWindow.vue
+++ src/components/mainWindow/fightWindow.vue
@@ -1,16 +1,46 @@
 <template>
   <div class="komplettesKampffenster"> 
-        <div class="KampffensterOben"> 
-              <div id="rinks">
+        <div class="KampffensterOben">   
+            <div class="left"> 
+              <div class="obenL"> 
+                <div class="PokeStatsL">
+                  PokeStatsL
+                </div>
+                <div class="HealthBarL"> 
+                  HealthBarL
+                </div>
               </div>
 
-              <div id="rechts">
+              <div class="untenL"> 
+                untenL
               </div>
+            </div>
+
+            <div class="right"> 
+              <div class="obenR"> 
+                <div class="gegnerPokeStatsR">
+                  <div class="gegnerInformationen">
+                    GegnerInformationen
+                  </div>
+
+                  <div class="healthbarGegner">
+                    healthbarGegner
+                  </div>
+                </div>
+
+                <div class="gegnerPokemonR">
+                  gegner Pokemon Sprite
+                </div>
+              </div>
+
+              <div class="untenR"> 
+                ubenR
+              </div>
+            </div>
         </div>
 
-
         <div class="konsoleUnten"> 
-          <p> hier steht text von der Konsole</p>
+            <p> hier steht text von der Konsole</p>
         </div>
   </div>
 </template>
@@ -25,13 +55,11 @@
     data(){ 
       return {
         data: {
-
         }
       }
     },
     methods: {
       showPokeInfo() {
-
       }
     }
 
@@ -40,4 +68,113 @@
   </script>
   
   <style>
+    .komplettesKampffenster{
+      height: 800px;
+      width: 1320px; 
+      background-color: rgb(0, 0, 0);
+      display: flex;
+      flex-direction: column;
+
+    }
+    .KampffensterOben{
+      background-color: rgb(238, 5, 5);
+      height: 700px;
+      display: flex;
+      flex-direction: row;
+
+    }
+    .konsoleUnten{
+      background-color: rgb(34, 157, 214);
+      height: 100px;
+
+}
+
+.left{
+      height: 100;
+      width: 800px;
+      display: flex;
+      flex-direction: column
+
+}
+
+.right{
+      background-color: rgb(185, 34, 135);
+      height: 100;
+      width: 520px;
+      display: flex;
+      flex-direction: column
+}
+
+.obenL{
+  background-color: rgb(185, 34, 34);
+  height: 210px;
+  width: 100;
+  display: flex;
+  flex-direction: column
+}
+
+.untenL{
+  background-color: rgb(59, 185, 34);
+  height: 490px;
+  width: 100;
+}
+
+.obenR{
+  background-color: rgb(196, 214, 34);
+  height: 490px;
+  width: 100;
+  display: flex;
+  flex-direction: column
+}
+
+.untenR{
+  background-color: rgb(236, 132, 14);
+  height: 210px;
+  width: 100;
+}
+
+.gegnerPokeStatsR{
+  width: 100;
+  height: 150px;
+  background-color: rgb(196, 214, 34);
+  display: flex;
+  flex-direction: column
+}
+
+.gegnerPokemonR{
+  width: 100;
+  height: 340px;
+  background-color: rgb(214, 34, 205);
+}
+
+.gegnerInformationen{
+  background-color: rgb(196, 214, 34);
+  width: 100;
+  height: 75px;
+}
+
+.healthbarGegner{
+  background-color: rgb(34, 157, 214);
+  width: 100;
+  height: 75px;
+}
+
+.PokeStatsL{
+  background-color: rgb(185, 34, 34);
+
+}
+.class.HealthBar{
+  background-color: rgb(167, 73, 221);
+}
+
+
+
+
+
+
+
+
+
+
+
   </style>
